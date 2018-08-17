7b37670d76fb42788ac2e7885b3c6ca54887a204
Deyvid <moraes.deyvid@gmail.com>
2018-08-17 01:29:43 -0300

    aula de roteamento entre as paginas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ola Mundo' });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Ola Mundo' });
+});
+
+router.post('/new', function(req, res, next) {
+	var nome = req.body.nome;
+	var idade = req.body.idade;
+    res.redirect('/?nome=' + nome );
 });
 
 module.exports = router;
